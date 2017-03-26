c3ba0d9a505379512f90ef4550fbb905e9b9bdc9
Siddhartha <Siddhartha@DESKTOP-QGTP803>
2017-03-26 11:08:21 +0600

    Initioal Commit

---
+++ Dec of Cards/src/Dec.java
@@ -0,0 +1,13 @@
+
+public class Dec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helo Sid");
+		int a = 5;
+		int b = 10;
+		int c;
+		c = a*b;
+		System.out.println(c);
+	}
+}
